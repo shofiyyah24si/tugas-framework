5e0bc8210fd901d8b5eedd00a0514da8a8637fba
shofiyyah <shofiyyah24si@mahasiswa.pcr.ac.id>
2025-09-30 18:47:45 +0700

    perubahan about

--- resources/views/halaman-about.blade.php
+++ resources/views/halaman-about.blade.php
@@ -5,6 +5,6 @@
 </head>
 <body>
     <h1>Ini adalah halaman About</h1>
-    <p>Laravel membuat development lebih cepattt  cobabhhjdc🚀</p>
+    <p>Laravel membuat development lebih cepattt  coba🚀</p>
 </body>
 </html>
